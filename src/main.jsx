82e29ee782b92b0b4e2f1f48e7b050aee453ed95
Raskollnikov <bakhalaia56@gmail.com>
2024-09-19 18:37:07 +0400

    setting files and installing important files

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/",
+    // element: ,
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
